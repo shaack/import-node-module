1ee44bfd328581133126079d2379812c264a4027
shaack <shaack@gmail.com>
2023-05-16 20:06:01 +0200

    renamed to import-node-modules

--- import-node-modules.js
+++ import-node-modules.js
@@ -3,7 +3,6 @@
  * Repository: https://github.com/shaack/node-modules-url
  * License: MIT, see file 'LICENSE'
  */
-if (!window.nodeModulesUrl) {
-    window.nodeModulesUrl = import.meta.url.substring(0, import.meta.url.lastIndexOf("/node_modules/") + 13)
-    window.node_modules = window.nodeModulesUrl
+if (!window.node_modules) {
+    window.node_modules = import.meta.url.substring(0, import.meta.url.lastIndexOf("/node_modules/") + 13)
 }
